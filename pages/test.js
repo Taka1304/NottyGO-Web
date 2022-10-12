4911873e4e7d43f1525d752f85b65be254133786
TakayaKatsuki <c1101036@planet.kanazawa-it.ac.jp>
2022-10-12 14:42:35 +0900

    確認用テストページを追加

---
+++ pages/test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../src/components/Footer'
+import Header from '../src/components/Header'
+
+const test = () => {
+  return (
+    <div>
+      <Header />
+       <p>test</p>
+      <Footer />
+    </div>
+  )
+}
+
+export default test
